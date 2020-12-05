dbac926930e9bec0771f568dcd06a6d9252b8cbd
japoch <jposvic@gmx.de>
2020-12-05 14:28:17 +0100

    webapp: small changes

--- src/play.py
+++ src/play.py
@@ -1,4 +1,5 @@
 from soco import SoCo, discover
+from lxml import etree
 
 if __name__ == '__main__':
     for zone in discover():
@@ -13,13 +14,13 @@ if __name__ == '__main__':
     #sonos.play_uri('http://192.168.178.39:5901/stream/swyh.mp3')
     #sonos.play_uri('http://ia801402.us.archive.org/20/items/TenD2005-07-16.flac16/TenD2005-07-16t10Wonderboy.mp3')
     # FM4.ORF.AT
-    sonos.play_uri('http://mp3stream1.apasf.apa.at:8000', title='FM4', force_radio=True)
+    #sonos.play_uri('http://mp3stream1.apasf.apa.at:8000', title='FM4', force_radio=True)
 
     track = sonos.get_current_track_info()
     print(f'URI={track["uri"]}')
 
     #sonos.pause()
-    sonos.play()
+    #sonos.play()
 
     sonos.volume = 45
     sonos.bass = 0
@@ -28,3 +29,13 @@ if __name__ == '__main__':
     print(f'Volume={sonos.volume}')
     print(f'Bass={sonos.bass}')
     print(f'Treble={sonos.treble}')
+    info = sonos.get_current_track_info()
+    print(f'Position={info["position"]}')
+
+    parser = etree.XMLParser(remove_blank_text=True, ns_clean=True)
+    metadata = etree.XML(info['metadata'], parser)
+    #metadata = etree.fromstring(info["metadata"], pretty_print=True)
+    #print(f'Metadata={etree.tostring(metadata)}')
+    for item in metadata.getiterator():
+        if item.tag.endswith('streamContent'):
+            print(f'streamContent={item.text}')
--- webapp.bat
+++ webapp.bat
@@ -2,5 +2,5 @@
 set "PYTHON_DIR=C:\Python38_64"
 set "PYTHON_BIN=%PYTHON_DIR%\python.exe"
 
-echo.--- run play.py
-"%PYTHON_BIN%" %~dp0/submodules/SoCo/examples/webapp/index.py
+echo.--- run webapp.py
+"%PYTHON_BIN%" %~dp0/src/webapp/index.py
