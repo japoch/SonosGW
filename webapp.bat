f26abd306370bf06eaae8e9a61dfb4b5794049ab
japoch <jposvic@gmx.de>
2020-12-14 17:11:08 +0100

    webapp: implementing the bootstrap framework

--- webapp.bat
+++ webapp.bat
@@ -1,6 +1,7 @@
 @echo off
 set "PYTHON_DIR=C:\Python38_64"
-set "PYTHON_BIN=%PYTHON_DIR%\python.exe"
+rem set "PYTHON_BIN=%PYTHON_DIR%\python.exe"
+set "PYTHON_BIN=python.exe"
 
 echo.--- run webapp.py
 "%PYTHON_BIN%" %~dp0/src/webapp.py
