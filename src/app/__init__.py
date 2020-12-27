956ec6d60a03e1ac677f3638a0dfd2a0cd33f930
japoch <jposvic@gmx.de>
2020-12-27 14:47:49 +0100

    webapp: Flask-Bootstrap removed

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,9 +2,7 @@
 This is the flask application for the Sonos Controller.
 """
 from flask import Flask
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
 from app import routes
